26eb6ed3c9093c4cc792e4d0e868ca291ce34995
j-agbaje <144043288+j-agbaje@users.noreply.github.com>
2024-11-28 14:18:29 +0200

    Add other files

---
+++ app.py
@@ -0,0 +1,15 @@
+from StudentConnect import app, db
+from StudentConnect import socketio
+
+# from StudentConnect.routes import config
+
+
+
+
+# print(config)
+# Run the app
+if __name__ == '__main__':  # Ensures app context is active   
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, port=8000)
+    
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+      './StudentConnect/templates/search.html', // Actual template file names
+      ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
