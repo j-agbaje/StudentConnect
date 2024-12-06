c32f7e8666a6a688175ffdc806fc32b47601cd41
j-agbaje <144043288+j-agbaje@users.noreply.github.com>
2024-12-07 02:25:47 +0300

    change_url

--- StudentConnect/routes.py
+++ StudentConnect/routes.py
@@ -97,7 +97,7 @@ except json.JSONDecodeError:
 
 
 es = Elasticsearch(
-    "https://6eb25373254e449fab3f18eac554e1e1.us-central1.gcp.cloud.es.io",
+    "https://6eb25373254e449fab3f18eac554e1e1.us-central1.gcp.cloud.es.io:443",
     api_key=os.getenv('ELASTICSEARCH_API_KEY'))
 
 
